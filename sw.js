212cd4a090719d5d854fb4bc46dc994ce9f78db6
Wander <wander.mi@gmail.com>
2026-05-28 18:17:24 -0300

    Atualização sw.js e manifest.jason

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
-const CACHE_NAME = 'wmb-gym-v2'; // ou v3, v4... incrementa
+const CACHE_NAME = 'wmb-gym-v2';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/sw.js', '/icon.svg'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(() => {})));
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS).catch(() => {})));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
   ));
   self.clients.claim();
 });
@@ -18,7 +18,7 @@ self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(r => r || fetch(e.request).then(res => {
       const clone = res.clone();
-      caches.open(CACHE).then(c => c.put(e.request, clone));
+      caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
       return res;
     }).catch(() => caches.match('/index.html')))
   );
